15ef93218139ee52da328d412633808090f50c1e
ranjanisrinivasan <ranjaniroxs@gmail.com>
2017-06-28 11:15:30 +0530

    Update leap_year_or_not.rb

--- leap_year_or_not.rb
+++ leap_year_or_not.rb
@@ -1,10 +1,13 @@
 def leap_year
-year = gets.chomp.to_i
+ year = gets.chomp.to_i
  if year% 400 == 0
  if year% 100 == 0
  if year% 4 == 0
-puts "leap year"
-end
-puts "not leap year"
-end puts "not leap year" end puts "leap year"
+  puts "leap year"
+ end
+  puts "not leap year"
+ end
+  puts "not leap year"
+ end
+ puts "leap year"
 end
