d6210b33592a3bd20f2c82d5cd7c0db2f90ada3d
ranjanisrinivasan <ranjaniroxs@gmail.com>
2017-06-28 11:10:51 +0530

    Update leap_year_or_not.rb

--- leap_year_or_not.rb
+++ leap_year_or_not.rb
@@ -1,9 +1,10 @@
-def year 
-puts "enter the year"
+def leap_year
 year = gets.chomp.to_i
- if(year%400==0)
- puts "leap year"
- else
- puts "not a leap year"
- end
+ if year% 400 == 0
+ if year% 100 == 0
+ if year% 4 == 0
+puts "leap year"
+end
+puts "not leap year"
+end puts "not leap year" end puts "leap year"
 end
