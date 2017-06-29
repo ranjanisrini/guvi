aa717d4af5c93ab3688d665327453059aeb5fbf4
ranjanisrinivasan <ranjaniroxs@gmail.com>
2017-06-29 21:17:22 +0530

    Add files via upload

---
+++ sum.rb
@@ -0,0 +1,11 @@
+n='a'
+until n == n.to_i.to_s && n.to_i > 0
+  puts "Enter the Natural number"
+  n = gets.chomp().strip()
+end
+n=n.to_i
+sum=0
+for i in (1..n) 
+sum+=i
+end
+puts "Sum of first #{n} natural numbers is #{sum}"
