7dadfa098ec9e36ded802de1499efb80dff08318
ranjanisrinivasan <ranjaniroxs@gmail.com>
2017-06-30 16:19:18 +0530

    Update mul.rb

--- mul.rb
+++ mul.rb
@@ -2,11 +2,11 @@ x = (1..12)
 y = (1..12)
 puts
 x.each {|i| puts "%-3d  " % i}
-print "\n     "
-x.each {|i| puts '---- '}
-puts "\n"
+puts
+x.each {|i| puts }
+puts 
 y.each do |j| 
-print "%-3d| " % j
+puts "%-3d| " % j
 x.each {|i| puts "%-3d  " % (i*j)}
 puts "\n"
 end
