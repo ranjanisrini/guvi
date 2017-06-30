c431cd8b705c85ba83c6c5724a6deece9af3041a
ranjanisrinivasan <ranjaniroxs@gmail.com>
2017-06-30 11:33:47 +0530

    Update even.rb

--- even.rb
+++ even.rb
@@ -1,8 +1,12 @@
-def odd
-a = gets.chomp.to_i
-n = gets.chomp.to_i
-for i in n..a do
+def even
+puts "enter the min number"
+min = gets.chomp.to_i
+puts "enter the max number"
+max = gets.chomp.to_i
+for i in min..max+1 do
+if(i%2==0)
 puts i
+end 
 end
-i.each do {|i| i.odd?}
 end
+
