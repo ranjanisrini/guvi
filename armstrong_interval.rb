467833a266dde4d8344efea32d42a7925279cd72
ranjanisrinivasan <ranjaniroxs@gmail.com>
2017-07-01 18:06:26 +0530

    Update armstrong_interval.rb

--- armstrong_interval.rb
+++ armstrong_interval.rb
@@ -1,18 +1,24 @@
-def get_interval
-  puts "enter the number"
-  a = gets.chomp.to_i
-  b = gets.chomp.to_i
-  for i in a..b do
-    new_num=n
-    sum=0
-  while(new_num>0)
-    sum=sum+(new_num%10)*(new_num%10)*(new_num%10)
-    new_num=new_num/10
- end
-  if sum==n
-    puts "Armstrong"
-  else
-    puts "non-Armstrong"
+def arm(number)
+z = number.to_s
+n = z.split('')
+a=n.map(&:to_i)
+a.length
+ x = n.map(&:to_i).map {|x| x ** (n.length) }
+ y = x.inject(0) { |sum,y| sum +y}
+if (  y== z.to_i)
+true
+else
+false
 end
+end
+
+def armstg
+puts "enter the min number"
+min = gets.chomp.to_i
+puts "enter the max number"
+max = gets.chomp.to_i
+for i in min..max do
+puts i if arm(i)
+end 
 end
 end
