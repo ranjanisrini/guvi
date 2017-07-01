54771b4ca5ae1c06d0eb88de870d3ab2c7b3ec64
ranjanisrinivasan <ranjaniroxs@gmail.com>
2017-07-01 18:05:25 +0530

    Update armstrong.rb

--- armstrong.rb
+++ armstrong.rb
@@ -1,16 +1,13 @@
-def get
-  puts "enter the number"
-    n=gets.chomp.to_i
-    sum=0
-new_num=n
-    while(new_num!=0)
-    sum=sum+(new_num%10)*(new_num%10)*(new_num%10)
-    new_num=new_num/10
- end
-  if sum==n
-    puts "Armstrong"
-  else
-    puts "non-Armstrong"
+def arm(number)
+z = number.to_s
+n = z.split('')
+a=n.map(&:to_i)
+a.length
+ x = n.map(&:to_i).map {|x| x ** (n.length) }
+ y = x.inject(0) { |sum,y| sum +y}
+if (  y== z.to_i)
+true
+else
+false
 end
 end
-
